6935955d13474078dbfe7b508ee0e20d4a26032d
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-11-03 16:39:41 +0000

    Update classes

--- app/src/main/java/com/ml/spaceflightapp/model/flight/Badge.kt
+++ app/src/main/java/com/ml/spaceflightapp/model/flight/Badge.kt
@@ -1,11 +1,13 @@
 package com.ml.spaceflightapp.model.flight
 
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
 /**
  * Class is used to provide images of the flight organization's logo
  */
+@Entity
 data class Badge (
-	@SerializedName("small") val small : String,
-	@SerializedName("large") val large : String
+	@SerializedName("small") val small : String?,
+	@SerializedName("large") val large : String?
 )
--- app/src/main/java/com/ml/spaceflightapp/model/flight/Links.kt
+++ app/src/main/java/com/ml/spaceflightapp/model/flight/Links.kt
@@ -1,12 +1,14 @@
 package com.ml.spaceflightapp.model.flight
 
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
 /**
  * This class provides access to media related to the flight
  */
+@Entity
 data class Links (
-	@SerializedName("badge") val badge : Badge,
+	@SerializedName("patch") val badge : Badge,
 	@SerializedName("presskit") val presskit : String,
 	@SerializedName("webcast") val webcast : String,
 	@SerializedName("youtube_id") val youtube_id : String,
